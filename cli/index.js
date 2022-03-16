7aefb7bab0a59f9dc28bb66b5bedf17b32b496f2
Sebastiano <sebastiano.gaudeano@live.com>
2022-03-16 11:22:39 +0100

    format

--- cli/index.js
+++ cli/index.js
@@ -6,15 +6,20 @@ import process from './macro.deploy';
 export async function macro(options) {
     console.log(options);
     let rooms = [];
+
     try {
+        
         const data = fs.readFileSync(options.source);
         rooms = JSON.parse(data);    
+
     } catch (error) {
         console.error("%s Can't read the room file", chalk.red.bold('ERROR'));
     }
+    
     options = {
         ...options,
         rooms: rooms
     }
+    
     await process(options);
 }
